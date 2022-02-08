971fd6714dbc71de670e9fbe559636c8a5fa14f7
Nina <Nino4ka11021988@gmail.com>
2022-02-08 11:30:56 +0300

    Implement mul function

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,11 +4,16 @@ public class Launcher {
         int b = 4;
         System.out.println(add(a,b));
         System.out.println(sub(a,b));
+        System.out.println(mul(a,b));
+
     }
     private static int add(int a , int b){
         return a+b;
     }
     private static int sub(int a , int b){
         return a-b;
+    }
+    private static int mul(int a , int b){
+        return a*b;
     }
 }
